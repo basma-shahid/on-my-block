2c32c295dd830ab2af834bab0f2384e195a5fdcf
Idahosa Ogieva <ida@Idahosas-MacBook-Air.local>
2021-08-01 19:41:16 -0400

    add 3 pages, 2 front end routes and their links via buttons

---
+++ src/pages/CreatePage/CreatePage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+export default function CreatePage() {
+    return (
+        <div>
+            This is the create page
+        </div>
+    )
+}
