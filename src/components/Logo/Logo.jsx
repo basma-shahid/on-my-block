eccba0744a50b6d60dce19e7bff107b6939ba2e5
Basma <basma@Basmas-Air.hitronhub.home>
2021-08-03 13:02:51 -0600

    Add logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css'
+
+function Logo() {
+  return (
+      <div>
+        <img className="logo" src="logo1.svg" />
+      </div>
+  );
+  }
+
+export default Logo;
