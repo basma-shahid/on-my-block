6d7a3940e7cdf49e6783f6af826e384e7bd34cd1
Idahosa Ogieva <ida@Idahosas-MacBook-Air.local>
2021-08-03 10:55:53 -0400

    formatting

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -27,6 +27,7 @@ export default class EventsPage extends Component{
                 
                 Welcome, {this.props.user.name}
                 <EventForm />
+                <Link className="btn" to='/profile'>View Profile</Link>
                 
 
             </div>
@@ -34,10 +35,10 @@ export default class EventsPage extends Component{
                 <Map lat={this.state.lat} lng={this.state.lng}/>
                 
             </div>
-        <div>
+        {/* <div>
             This is the events page
-            <Link className="btn" to='/profile'>View Profile</Link>
-        </div>
+            
+        </div> */}
         </div>
     )
     }
