65591a0967dfd7ae816695c0ca06634b8cb407bb
Basma <basma@Basmas-Air.hitronhub.home>
2021-08-04 13:55:24 -0600

    Minor update

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,14 +1,12 @@
 import './ProfilePage.css';
 import React from 'react';
 import UserInfo from '../../components/UserInfo/UserInfo';
-import ImageUpload from '../../components/ImageUpload/ImageUpload';
 
 export default class ProfilePage extends React.Component {
 
   render() {
     return (
       <main className="profile">
-        {/* <ImageUpload /> */}
         <h2>Name: {this.props.user.name}</h2> <br/>
         <h2>Email: {this.props.user.email}</h2>
         <br/><br/>
